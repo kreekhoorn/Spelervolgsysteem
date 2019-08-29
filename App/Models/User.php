a9de3a3faba8a780880906034d2ed6817be9758d
theomen3 <33961419+theomen3@users.noreply.github.com>
2019-08-29 15:17:34 +0200

    Add files via upload

--- App/Models/User.php
+++ App/Models/User.php
@@ -9,18 +9,81 @@ use PDO;
  *
  * PHP version 7.0
  */
-class User extends \Core\Model
+class User extends Model
 {
+    /**
+     * Error messages
+     *
+     * @var array
+     */
+    public $errors = [];
+
+    /**
+     * Class constructor
+     *
+     * @param array $data Initial property values
+     *
+     * @return void
+     */
+    public function __construct($data = [])
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        };
+        /*        $this->activity = Interest::getInterest();*/
+    }
 
     /**
      * Get all the users as an associative array
      *
      * @return array
      */
+
     public static function getAll()
     {
         $db = static::getDB();
         $stmt = $db->query('SELECT id, name FROM users');
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+    /**
+     * @param $email email adres to serach for
+     * @return mixed User object if found, false otherwise
+     */
+    public static function findByEmail($email)
+    {
+        $sql = 'SELECT * FROM users WHERE email = :email';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+
+    /**
+     * Authenticate a user by email and password.
+     *
+     * @param string $email email address
+     * @param string $password password
+     *
+     * @return mixed  The user object or false if authentication fails
+     */
+    public static function authenticate($email, $password)
+    {
+        $user = static::findByEmail($email);
+
+        if ($user && $user->is_active)
+            if (password_verify($password, $user->password_hash)) {
+                return $user;
+
+            }
+
+        return false;
     }
 }
